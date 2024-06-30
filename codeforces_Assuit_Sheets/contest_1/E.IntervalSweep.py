6d6010c48a026d426161e67a441774543c157793
Aya Ragab <119764784+ayaarragab@users.noreply.github.com>
2024-06-30 13:26:34 +0300

    contest 1

--- codeforces_Assuit_Sheets/contest_1/E.IntervalSweep.py
+++ codeforces_Assuit_Sheets/contest_1/E.IntervalSweep.py
@@ -1,7 +1,7 @@
 def main():
     nums = input().split()
     num1, num2 = int(nums[0]), int(nums[1])
-    if abs(num1 - num2) == 1 or num1 - num2 == 0:
+    if abs(num1 - num2) == 1 or (num1 - num2 == 0 and (num1 != 0 or num2 != 0)):
         print("YES")
     else:
         print("NO") 
